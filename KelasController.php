d5a19ccecc33efd35cb60bd386509ba57b14c7c7
aqila13 <63911057+aqila13@users.noreply.github.com>
2020-12-14 22:33:37 +0700

    Update KelasController.php

--- KelasController.php
+++ KelasController.php
@@ -30,4 +30,32 @@ public function show(){
             return Response()->json(['status'=>0]);
         }
     }
+    public function update($id, Request $request)
+    {
+        $validator=Validator::make($request->all(),
+            [
+                'nama_kelas' => 'required'
+            ]
+        );
+        if($validator->fails()) {
+            return Response()->json($validator->errors());
+        }
+        $ubah = Kelas::where('id', $id)->update([
+            'nama_kelas' => $request->nama_kelas
+        ]);
+        if($ubah) {
+            return Response()->json(['status' => 1]);
+        }else {
+            return Response()->json(['status' => 0]);
+        }
+    }
+    public function destroy($id)
+    {
+        $hapus = Kelas::where('id', $id)->delete();
+        if($hapus) {
+            return Response()->json(['status' => 1]);
+        }else {
+            return Response()->json(['status' => 0]);
+    }
+ }
 }
